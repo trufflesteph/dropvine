9b63979a403b815d75c60c508364569b56fd0b52
trufflesteph <stephanie.liefeld@gmail.com>
2026-05-11 16:14:49 -0700

    Reapply full codebase changes

--- next.config.js
+++ next.config.js
@@ -3,15 +3,24 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    // Remove if not using Server Components
+    serverComponentsExternalPackages: ['mongodb'],
+  },
   webpack(config, { dev }) {
     if (dev) {
+      // Reduce CPU/memory from file watching
       config.watchOptions = {
-        poll: 2000,
-        aggregateTimeout: 300,
+        poll: 2000, // check every 2 seconds
+        aggregateTimeout: 300, // wait before rebuilding
         ignored: ['**/node_modules'],
       };
     }
     return config;
+  },
+  onDemandEntries: {
+    maxInactiveAge: 10000,
+    pagesBufferLength: 2,
   },
   async headers() {
     return [
@@ -28,4 +37,5 @@ const nextConfig = {
     ];
   },
 };
+
 module.exports = nextConfig;
